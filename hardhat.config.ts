12324c69ea666bd0a72d35d6b56bc13e2c47a161
Noah Marconi <noah.marconi@gmail.com>
2021-03-24 09:49:46 -0400

    snapshot with role

---
+++ hardhat.config.ts
@@ -0,0 +1,57 @@
+import { task, HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import { Contract } from "ethers";
+
+require("dotenv").config();
+
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("deploy", "Deploys new token contract")
+  .setAction(async (args, hre) => {
+    // We get the contract to deploy
+    const ForceToken = await hre.ethers.getContractFactory("ForceToken");
+    const forceToken = await ForceToken.deploy("Force DAO", "FORCE");
+
+    console.log("Force DAO Token deployed to:", forceToken.address);
+  });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+      gasPrice: 8000000000,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${MAINNET_PRIVATE_KEY}`],
+    },
+    ganache: {
+      url: "http://127.0.0.1:8555",
+    },
+  },
+  solidity: "0.7.6",
+};
+
+export default config;
